18a4d824b0eb2742edaaf3ceb8192fd1a0f82e43
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-10-15 13:04:00 +1300

    working on home page

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function showMenu(event) {
    mobileMenu.classList.toggle("active");
+   modoNocturno.innerHTML = "Modo Nocturno";
+  misGifos.innerHTML = "Mis Gifos";
+  favoritos.innerHTML = "Favoritos";
 }
 let mobileMenu = document.querySelector(".menu");
 let burger = document.querySelector(".burger");
+let modoNocturno = document.querySelector(".modo-nocturno");
+let misGifos = document.querySelector(".mis-gifos");
+let favoritos = document.querySelector(".favoritos");
 burger.addEventListener("click", showMenu);
