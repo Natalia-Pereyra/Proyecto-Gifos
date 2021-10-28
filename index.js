b36c23aead62ba0aa26ee34679bd88bbb2abfee4
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-10-29 10:08:56 +1300

    Added containers-working on searbar

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ let burger = document.querySelector(".burger");
 let modoNocturno = document.querySelector(".modo-nocturno");
 let misGifos = document.querySelector(".mis-gifos");
 let favoritos = document.querySelector(".favoritos");
+
 burger.addEventListener("click", showMenu);
+
+function searchForm(event) {
+  event.preventDefault();
+ 
+  
+  console.log(searchTextInput);
+  // let h2 = document.querySelector("h2");
+
+  // h2.innerHTML = `Searching for ${searchTextInput}`;
+}
+let searchTextInput = document.querySelector("#search-text-input");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchForm);
