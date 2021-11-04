ae93b3f3c118e1ea79c75366aa9b6fdef8656c2c
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-11-04 17:35:46 +1300

    Working on responsiveness

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function showMenu(event) {
    mobileMenu.classList.toggle("active");
    menuImages.classList.toggle("open");
-   modoNocturno.innerHTML = "Modo Nocturno";
-  misGifos.innerHTML = "Mis Gifos";
-  favoritos.innerHTML = "Favoritos";
+  //  modoNocturno.innerHTML = "Modo Nocturno";
+  // misGifos.innerHTML = "Mis Gifos";
+  // favoritos.innerHTML = "Favoritos";
 }
 
 function changeModoNocturno(event) {
   event.preventDefault();
   document.body.classList.toggle("body-modo-nocturno");
   modoNocturno.classList.toggle("hide-modo-nocturno");
-  modoNocturno.setAttribute("src", "./Images/logo-mobile-modo-noct.svg");
+  // modoNocturno.setAttribute("src", "./Images/logo-mobile-modo-noct.svg");
   modoDiurno.classList.toggle("show-modo-diurno");
+}
+
+function showMenuModoNocturno(event) {
+  event.preventDefault();
+  mobileMenu.classList.toggle("active", "body-modo-nocturno");
+
+
 }
 
 let mobileMenu = document.querySelector(".menu");
@@ -19,9 +26,11 @@ let burger = document.querySelector(".burger");
 let cruz = document.querySelector(".cruz");
 let menuImages = document.querySelector(".menu-imgs");
 let modoNocturno = document.querySelector(".modo-nocturno");
-let misGifos = document.querySelector(".mis-gifos");
-let favoritos = document.querySelector(".favoritos");
-let modoDiurno = document.querySelector(".modo-diurno")
+// let misGifos = document.querySelector(".mis-gifos");
+// let favoritos = document.querySelector(".favoritos");
+let modoDiurno = document.querySelector(".modo-diurno");
+let burgerModoNocturno = document.querySelector(".burger-modo-noc");
+let cruzModoNocturno = document.querySelector(".cruz-modo-noc");
 
 
 
@@ -31,15 +40,18 @@ modoDiurno.addEventListener("click", changeModoNocturno);
 burger.addEventListener("click", showMenu);
 cruz.addEventListener("click", showMenu);
 
-function searchForm(event) {
-  event.preventDefault();
+burgerModoNocturno.addEventListener("click", showMenuModoNocturno);
+cruzModoNocturno.addEventListener("click", showMenuModoNocturno);
+
+// function searchForm(event) {
+//   event.preventDefault();
  
   
-  console.log(searchTextInput);
-  // let h2 = document.querySelector("h2");
+//   console.log(searchTextInput);
+//   // let h2 = document.querySelector("h2");
 
-  // h2.innerHTML = `Searching for ${searchTextInput}`;
-}
-let searchTextInput = document.querySelector("#search-text-input");
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchForm);
+//   // h2.innerHTML = `Searching for ${searchTextInput}`;
+// }
+// let searchTextInput = document.querySelector("#search-text-input");
+// let form = document.querySelector("#search-form");
+// form.addEventListener("submit", searchForm);
