99f6e20cf5437786adb69051bc1005dc3b56e671
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-12-07 09:32:14 +1300

    Added results gallery

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var mobileMenu = document.querySelector(".menu");
+var burger = document.querySelector(".burger");
+var cruz = document.querySelector(".cruz");
+var menuImages = document.querySelector(".menu-imgs");
+var modoNocturno = document.querySelector(".modo-nocturno");
+var modoDiurno = document.querySelector(".modo-diurno");
+var burgerModoNocturno = document.querySelector(".burger-modo-noc");
+var cruzModoNocturno = document.querySelector(".cruz-modo-noc");
+var searchIcon = document.getElementsByClassName("search-icon");
+
+
 function showMenu(event) {
    mobileMenu.classList.toggle("active");
    menuImages.classList.toggle("open");
@@ -17,53 +28,149 @@ function showMenuModoNocturno(event) {
   burger.classList.toggle("modo-diurno");
 }
 
-let mobileMenu = document.querySelector(".menu");
-let burger = document.querySelector(".burger");
-let cruz = document.querySelector(".cruz");
-let menuImages = document.querySelector(".menu-imgs");
-let modoNocturno = document.querySelector(".modo-nocturno");
-let modoDiurno = document.querySelector(".modo-diurno");
-let burgerModoNocturno = document.querySelector(".burger-modo-noc");
-let cruzModoNocturno = document.querySelector(".cruz-modo-noc");
-
-
-
 modoNocturno.addEventListener("click", changeModoNocturno);
 modoDiurno.addEventListener("click", changeModoNocturno);
-
 burger.addEventListener("click", showMenu);
 cruz.addEventListener("click", showMenu);
-
 burgerModoNocturno.addEventListener("click", showMenuModoNocturno);
 cruzModoNocturno.addEventListener("click", showMenuModoNocturno);
 
 
+var modal = document.getElementById("modal");
+var iconAumentarOverlay = document.getElementById("hover-aumentar");
+var modalSpan = document.getElementsByClassName("close-modal")[0];
+
 function showModal() {
+  let imageAumentada = document.getElementsByClassName("image-aumentada");
   modal.style.display = "block";
+
 }
 
 function closeModal() {
   modal.style.display = "none";
 }
 
-let modal = document.getElementById("modal");
-let iconDescargarOverlay = document.getElementById("hover-aumentar");
-let modalSpan = document.getElementsByClassName("close-modal")[0];
-iconDescargarOverlay.addEventListener("click", showModal);
+iconAumentarOverlay.addEventListener("click", showModal);
 modalSpan.addEventListener("click", closeModal);
 
 
-function showInput(event) {
-event.preventDefault();
-let input = document.querySelector("#search-text-input");
-let apiKey = `&api_key=OmE7QZS97YExac8Bv5bjnEPvgPK9fhh8`;
-let apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}&q=${input.value}`;
 
-fetch(apiUrl)
-.then(response => response.json())
-.then(data => console.log(data.data[0].images.original.url))
-.catch(err => console.log(err));
+
+function trendingExamplesHome() {
+
+  let trendingHome = document.getElementById("trending-examples");
+  let apiKey = `&api_key=OmE7QZS97YExac8Bv5bjnEPvgPK9fhh8`;
+  let trendingHomeHTML = '';
+  let apiTrending = `https://api.giphy.com/v1/trending/searches?${apiKey}`;
+
+  fetch(apiTrending)
+  .then(response => response.json())
+  .then(data => {
+
+    for (let i = 0; i < 5; i++) {
+      trendingHomeHTML += `${data.data[i]}, `;
+    }
+    trendingHome.innerHTML = trendingHomeHTML;
+  });
+}
+trendingExamplesHome();
+
+
+var form = document.querySelector("#search-form");
+var searchValue = "";
+var apiKey = `&api_key=OmE7QZS97YExac8Bv5bjnEPvgPK9fhh8`;
+var input = document.querySelector("#search-text-input");
+
+function showInput() {
+  let apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}&q=${searchValue}&limit=12`;
+  let resultsContainer = document.getElementById("results-container");
+  let resultsContainerHTML = '';
+  let title = document.getElementById("title");
+  
+
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    resultsContainer.innerHTML = "";
+     data.data.forEach(function (input) {
+            var templateImage = document.createElement("img");
+            templateImage.classList.add("imagen-prueba");
+            templateImage.src = `${input.images.fixed_width.url}`;
+
+            let resultTemplateDiv = document.createElement("div");
+            resultTemplateDiv.classList.add("result-template");
+            resultsContainer.appendChild(resultTemplateDiv);
+            resultTemplateDiv.appendChild(templateImage);
+             
+            let overlayDiv = document.createElement("div");
+            overlayDiv.classList.add("overlay");
+            resultTemplateDiv.appendChild(overlayDiv);
+
+            let overlayLinksDiv = document.createElement("div");
+            overlayLinksDiv.classList.add("overlay-links");
+            overlayDiv.appendChild(overlayLinksDiv);
+
+            let overlayLinkFav = document.createElement("img");
+            let overlayLinkDescarga = document.createElement("img");
+            let overlayLinkFullScreen = document.createElement("img");
+            overlayLinkFav.classList.add("hover-favoritos");
+            overlayLinkFav.src = "./Images/icon-fav.svg";
+            overlayLinkDescarga.classList.add("hover-descargar");
+            overlayLinkDescarga.src = "./Images/icon-download.svg"
+            overlayLinkFullScreen.classList.add("hover-aumentar");
+            overlayLinkFullScreen.src = "./Images/icon-max-normal.svg";
+            overlayLinksDiv.append(overlayLinkFav, overlayLinkDescarga, overlayLinkFullScreen);
+
+            let pElementOne = document.createElement("p");
+            pElementOne.classList.add("overlay-titulo-GIFO");
+            pElementOne.innerText = input.value;
+            let pElementTwo = document.createElement("p");
+            pElementTwo.classList.add("overlay-user");
+             overlayDiv.append(pElementOne, pElementTwo);
+    
+            console.log(input);
+        })
+        title.innerHTML = input.value;
+        
+  })
+  .catch(err => console.log(err));
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", showInput);
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  searchValue = input.value;
+  showInput();
+});
+
+function showSuggestions() {
+   let ulElement = document.getElementById("suggestions");
+   ulElement.style.display = "block";
+   let apiUrlSuggestions = `https://api.giphy.com/v1/gifs/search/tags?${apiKey}&q=${input.value}&limit=5`;
+
+   fetch(apiUrlSuggestions)
+   .then(response => response.json())
+   .then(data => {
+     ulElement.innerHTML = "";
+     data.data.forEach(function(tag) {
+
+       let newLi = document.createElement("li");
+       let newImage = document.createElement("img");
+       newLi.classList.add("list-suggestions");
+       newImage.classList.add("search-icon-gris");
+       newImage.src = "./Images/search.svg";
+
+       newLi.append(newImage);
+       newLi.innerHTML += tag.name;
+       newLi.addEventListener("click", function() {
+         searchValue = tag.name;
+         showInput();
+       })
+       ulElement.append(newLi);
+     })
+   })
+   .catch(err => console.log(err));
+}
+
+input.addEventListener("keyup", showSuggestions);
+// searchIcon.addEventListener("click", showInput);
+
--- prueba.js
+++ prueba.js
@@ -1,77 +1,163 @@
+var input = document.querySelector("#search-text-input");
+var apiKey = `&api_key=OmE7QZS97YExac8Bv5bjnEPvgPK9fhh8`;
+
+var form = document.querySelector("#search-form");
+var searchValue = "";
 
 function trendingExamplesHome() {
   let trendingHome = document.getElementById("trending-examples");
   let apiKey = `&api_key=OmE7QZS97YExac8Bv5bjnEPvgPK9fhh8`;
   let trendingHomeHTML = '';
   let trendingImages = document.getElementsByClassName("gallery-img");
+  let apiTrending = `https://api.giphy.com/v1/trending/searches?${apiKey}`;
   
-  let apiTrending = `https://api.giphy.com/v1/gifs/trending?${apiKey}&q=cat&limit=5`;
-  fetch(apiTrending)
+     fetch(apiTrending)
   .then(response => response.json())
-  .then(data => {
-    data.data.forEach(function(obj) {
-      console.log(obj.title);
-      trendingHomeHTML += `${obj.title}, `;
-      
-    })
+  .then(data => { 
+
+  for (let i = 0; i < 5 ; i++) {
+      trendingHomeHTML += `${data.data[i]}, `;
+  }
     trendingHome.innerHTML = trendingHomeHTML;
   });
 }
 trendingExamplesHome(); 
 
 
-function showInput(event) {
-  event.preventDefault();
-  let input = document.querySelector("#search-text-input");
-  let apiKey = `&api_key=OmE7QZS97YExac8Bv5bjnEPvgPK9fhh8`;
-  let apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}&q=${input.value}&limit=12`;
-  let imageContainer = document.getElementsByClassName("image-container");
-  let imageContainerHTML = '';
-  let h4 = document.getElementById("title");
-  let imagenPrueba = document.getElementById("imagen-prueba");
-  
-  
-  fetch(apiUrl)         
-   .then(response => response.json())
-   .then(data =>  {
-     data.data.forEach(function (input) {
-       console.log(input.images.original.url)
-       imageContainerHTML += `${input.images.original.url}`;
+function showInput() {
+    
+    var apiUrl = `https://api.giphy.com/v1/gifs/search?${apiKey}&q=${searchValue}&limit=12`;
+    let resultsContainer = document.getElementById("results-container");
+    let resultsSection = document.getElementById("results");
+    let title = document.getElementById("title");
+    
+    
+fetch(apiUrl)         
+    .then(response => response.json())
+    .then(data =>  {
+                resultsContainer.innerHTML = "";
+        data.data.forEach(function (input) {
+            
+
+            var templateImage = document.createElement("img");
+            templateImage.classList.add("imagen-prueba");
+            templateImage.src = `${input.images.original.url}`;
+
+            let resultTemplateDiv = document.createElement("div");
+            resultTemplateDiv.classList.add("result-template");
+            resultsContainer.appendChild(resultTemplateDiv);
+            resultTemplateDiv.appendChild(templateImage);
+             
+            let overlayDiv = document.createElement("div");
+            overlayDiv.classList.add("overlay");
+            resultTemplateDiv.appendChild(overlayDiv);
+
+            let overlayLinksDiv = document.createElement("div");
+            overlayLinksDiv.classList.add("overlay-links");
+            overlayDiv.appendChild(overlayLinksDiv);
+
+            let overlayLinkFav = document.createElement("img");
+            let overlayLinkDescarga = document.createElement("img");
+            let overlayLinkFullScreen = document.createElement("img");
+            overlayLinkFav.classList.add("hover-favoritos");
+            overlayLinkFav.src = "./Images/icon-fav.svg";
+            overlayLinkDescarga.classList.add("hover-descargar");
+            overlayLinkDescarga.src = "./Images/icon-download.svg"
+            overlayLinkFullScreen.classList.add("hover-aumentar");
+            overlayLinkFullScreen.src = "./Images/icon-max-normal.svg";
+            overlayLinksDiv.append(overlayLinkFav, overlayLinkDescarga, overlayLinkFullScreen);
+
+            let pElementOne = document.createElement("p");
+            pElementOne.classList.add("overlay-titulo-GIFO");
+            pElementOne.innerText = input.value;
+            let pElementTwo = document.createElement("p");
+            pElementTwo.classList.add("overlay-user");
+            overlayDiv.append(pElementOne, pElementTwo);
+    
+            console.log(input);
+        })
+        title.innerHTML = input.value;
+        
      })
-     imagenPrueba.setAttribute("src", imageContainerHTML);
-     h4.innerHTML = input.value;
-     })
-  .catch(err => console.log(err));
+    .catch(err => console.log(err));
    }
 
-// let oldNode = document.getElementsByClassName("image-container");
-// let newNode = oldNode.cloneNode();
-// newNode.find("#image-prueba").src = data[0].images.original.url;
-// let contenedor = getElementsByClassName("gallery-img");
-// contenedor.append(newNode);  
 function showSuggestions() {
-document.getElementById("suggestions").style.display = "block"
+    
+    var ulElement = document.getElementById("suggestions");
+    ulElement.style.display = "block";
+    let apiUrlSuggestions = `https://api.giphy.com/v1/gifs/search/tags?${apiKey}&q=${input.value}&limit=5`;
+    
+   fetch(apiUrlSuggestions)         
+   .then(response => response.json())
+   .then(data =>  {
+       ulElement.innerHTML = "";
+     data.data.forEach(function (tag) {
+         
+        let newLi = document.createElement("li");
+        let newImage = document.createElement("img");
+        
+        newLi.classList.add("list-suggestions");
+        newImage.classList.add("search-icon-gris");
+        newImage.src = "./Images/search.svg";
+
+        newLi.append(newImage);
+        newLi.innerHTML += tag.name;
+        newLi.addEventListener("click", function() {
+        searchValue = tag.name;
+        showInput();
+        })
+        ulElement.append(newLi);
+     })
+    
+     })
+  .catch(err => console.log(err));
 }
 
-
-let form = document.querySelector("#form");
-form.addEventListener("submit", showInput);
-form.addEventListener("onkeyup", showSuggestions);
-
-
-
-// function abrirModal() {
-// modal.style.display = "block";
-// }
-// function cerrarModal() {
-//   modal.style.display = "none";
-// }
-
-// let modal = document.getElementById("modal");
-// let image = document.getElementById("image");
-// let span = document.getElementsByClassName("close-modal")[0];
-// image.addEventListener("click", abrirModal);
-// span.addEventListener("click", cerrarModal);
+form.addEventListener("submit",function(event) {
+    event.preventDefault();
+    searchValue = input.value;
+    showInput();
+});
+input.addEventListener("keyup", showSuggestions);
 
 
-  
+function trendingGallery() {
+     let apiTrending = `https://api.giphy.com/v1/gifs/search?${apiKey}`;
+     let imageContainer = document.createElement("div");
+     imageContainer.classList.add("image-container");
+    let overlayDiv = document.createElement("div");
+     overlayDiv.classList.add("overlay");
+     let trendingGifos = document.createElement("img");
+     trendingGifos.classList.add("gallery-img");
+     imageContainer.append(trendingGifos, overlayDiv);
+
+     fetch(apiTrending) 
+     .then(response => response.json())
+     .then(data => {
+      
+      for(let i = 0 ; i < 3; i++) {
+        trendingGifos.src += `${data.data[i]}`;
+            }
+            console.log(data);
+     });
+}
+trendingGallery();
+ 
+  //       <div class="image-container">
+  //           <img class="gallery-img" id="prueba-modal" src="./Images/card-busqueda-13.png" alt="">
+    
+  //           <div class="overlay">
+  //             <div class="overlay-links">
+  //               <a href="#"><img src="./Images/icon-fav.svg" alt="Agregar a favoritos"></a>
+  //               <a href="#"><img src="./Images/icon-download.svg" alt="Descargar GIFO"></a>
+  //               <a href="#"><img src="./Images/icon-max-normal.svg" alt="Aumentar GIFO"></a>
+  //             </div>
+  //             <p class="overlay-titulo-GIFO"><strong>título GIFO</strong></p>
+  //             <p class="overlay-user">User</p>
+  //           </div>
+  //         </div> 
+
+
+
+
